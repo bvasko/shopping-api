ba55b5c0a1a06b1db6aecd649dd951855e06e14d
Bonnie DiPasquale <bonnie.the.builder@icloud.com>
2022-01-23 16:39:15 -0500

    setup models

--- models/Category.js
+++ models/Category.js
@@ -7,12 +7,21 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
-    underscored: true,
     modelName: 'category',
   }
 );
